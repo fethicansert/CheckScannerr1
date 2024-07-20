5eb9c823608836430340f9f74a8d175cf4e06b78
unknown <fethisert0@gmail.com>
2024-07-20 19:13:40 +0300

    no message

--- my-app/src/components/checkscan-page/ScannerDetail.js
+++ my-app/src/components/checkscan-page/ScannerDetail.js
@@ -7,13 +7,13 @@ const ScannerDetails = ({checkScan}) =>{
         <div className="scanner-details">
             <div className="scanner-details-flex">
                 <h3 className="scanner-details-title">Çek Okuma</h3>
-                <label>Çek Okuyucu Durumu: <span className="check-status-text">Hazır</span></label>
+                <label  className="check-detail-label">Çek Okuyucu Durumu: <span className="check-status-text">Hazır</span></label>
             </div>
 
 
             <div className="scanner-details-flex">
                 <button  onClick={checkScan} className="check-image-button">Çek Oku</button>
-                <label>Çek Okuyucu Türü: <span className="check-scanner-type-text">DCC-TS240</span></label>
+                <label  className="check-detail-label">Çek Okuyucu Türü: <span className="check-scanner-type-text">DCC-TS240</span></label>
             </div>
             
         </div>
